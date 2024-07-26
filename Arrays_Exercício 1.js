aea64201669c75a2c7f313fbe1a085b22f563346
Mikaela Lazzarotto <mikaelazzarotto@gmail.com>
2024-07-26 15:58:03 -0300

    Create Arrays_Exercício 1.js

---
+++ Arrays_Exercício 1.js
@@ -0,0 +1,5 @@
+racasCachorro = [ "chow chow", "husky", "labrador", "golden retriever" , "pug"]
+
+numero = prompt ("Insira um número de 0 a 4:")
+
+console.log("A raça escolhida foi" , racasCachorro[numero])
