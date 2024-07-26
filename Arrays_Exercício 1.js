78d789ac165799e550b68f85e86f114c56c679f2
Mikaela Lazzarotto <mikaelazzarotto@gmail.com>
2024-07-26 15:58:58 -0300

    Update Arrays_Exercício 1.js

--- Arrays_Exercício 1.js
+++ Arrays_Exercício 1.js
@@ -1,5 +1,5 @@
+/*Crie um array com pelo menos 5 raças de cachorro. Peça para o usuário inserir um número de 0 a 4. Imprima no console a raça correspondente à posição escolhida pelo usuário*/
+
 racasCachorro = [ "chow chow", "husky", "labrador", "golden retriever" , "pug"]
-
 numero = prompt ("Insira um número de 0 a 4:")
-
 console.log("A raça escolhida foi" , racasCachorro[numero])
