3772bca9b165571ef1a3a906d4dbc8b28389a3ec
Mikaela Lazzarotto <mikaelazzarotto@gmail.com>
2024-07-27 11:01:59 -0300

    Create Arrays_Desafio 1.js

---
+++ Arrays_Desafio 1.js
@@ -0,0 +1,5 @@
+/*Receba uma frase e retorne um array onde cada elemento é uma das palavras da frase, ignorando os espaços*/
+
+let frase = prompt ("Digite uma frase:")
+let array = frase.split(" ");
+console.log(array)
