fe10874d33376f3bf1201acd4351de8d0c1acfe3
Mikaela Lazzarotto <mikaelazzarotto@gmail.com>
2024-07-27 11:02:25 -0300

    Update Arrays_Desafio 1.js

--- Arrays_Desafio 1.js
+++ Arrays_Desafio 1.js
@@ -1,5 +1,5 @@
 /*Receba uma frase e retorne um array onde cada elemento é uma das palavras da frase, ignorando os espaços*/
 
 let frase = prompt ("Digite uma frase:")
-let array = frase.split(" ");
+let array = frase.split(" "); // precisa colocar entre aspas duplas para retornar as palavras como elementos
 console.log(array)
